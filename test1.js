01fb215bb4547ea0d4dcf40b5d5236114398ad77
Pargeo82 <57045809+Pargeo82@users.noreply.github.com>
2021-11-16 19:24:41 +0100

    add js notes

--- test1.js
+++ test1.js
@@ -1,12 +1,20 @@
 const d = new Date();
+
+// set days
 const day = ['Nedjelja', 'Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota'];
+let currentDay = day[d.getDay()];
+
+// function that adds 0 infront on 1 digit numbers
 const addZero = (num) => {
     if (num < 10) {
         return '0' + num
     } else return num
 };
-let currentDay = day[d.getDay()];
+
+// time variable
 let currentTime = `${addZero(d.getHours())} : ${addZero(d.getMinutes())} : ${addZero(d.getSeconds())}`;
+
+// get the numbers into spans in html
 document.getElementById('currDay').innerHTML = currentDay;
 document.getElementById('currTime').innerHTML = currentTime;
 
